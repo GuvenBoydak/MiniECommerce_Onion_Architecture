60c524c566110125790477dc0657f4fa684fbd8e
Guven Boydak <gvn.boydak@gmail.com>
2022-10-24 18:00:22 +0300

    write repository methods edit

--- Infrastructure/MiniECommerce.Persistance/Repositories/WriteRepository.cs
+++ Infrastructure/MiniECommerce.Persistance/Repositories/WriteRepository.cs
@@ -7,12 +7,10 @@ namespace MiniECommerce.Persistance
     public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
     {
         protected readonly MiniECommerceDbContext _db;
-        private readonly IUnitOfWork _unitOfWork;
 
-        public WriteRepository(MiniECommerceDbContext db, IUnitOfWork unitOfWork)
+        public WriteRepository(MiniECommerceDbContext db)
         {
             _db = db;
-            _unitOfWork = unitOfWork;
         }
 
         public DbSet<T> Table => _db.Set<T>();
@@ -20,14 +18,12 @@ namespace MiniECommerce.Persistance
         public async Task AddAsync(T entity)
         {
              await Table.AddAsync(entity);
-            await _unitOfWork.SaveAsync();
         }
 
         public async Task DeleteAsync(Guid id)
         {
            T entity = await Table.FindAsync(id);
             entity.Status = DataStatus.Deleted;
-            await _unitOfWork.SaveAsync();
         }
 
         public async Task Update(T entity)
@@ -35,8 +31,6 @@ namespace MiniECommerce.Persistance
             T updatedEntity = await Table.FindAsync(entity.ID);
             updatedEntity.Status = DataStatus.Updated;
             Table.Update(entity);
-
-            await _unitOfWork.SaveAsync();
         }
     }
 }
