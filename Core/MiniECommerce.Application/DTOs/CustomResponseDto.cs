5d17df2f1167afc6127bb3060e9debbe5986ba9e
Guven Boydak <gvn.boydak@gmail.com>
2022-10-24 21:48:56 +0300

    CustomResponceDto, NoContenDto and  responce exception classes  added

---
+++ Core/MiniECommerce.Application/DTOs/CustomResponseDto.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MiniECommerce.Application
+{
+    public class CustomResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        public List<string> Error { get; set; }
+
+        public string Message { get; set; }
+
+        //static Factory Method Dessing Pattern.
+        public static CustomResponseDto<T> Success(int statusCode, T data, string message)
+        {
+            return new CustomResponseDto<T>() { Data = data, StatusCode = statusCode, Message = message };
+        }
+
+        public static CustomResponseDto<T> Success(int statusCode, string message)
+        {
+            return new CustomResponseDto<T>() { StatusCode = statusCode, Message = message };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, List<string> errors, string message)
+        {
+            return new CustomResponseDto<T>() { StatusCode = statusCode, Error = errors, Message = message };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string error, string message)
+        {
+            return new CustomResponseDto<T>() { StatusCode = statusCode, Error = new List<string> { error }, Message = message };
+        }
+
+        public static CustomResponseDto<T> Fail(int statusCode, string message)
+        {
+            return new CustomResponseDto<T>() { StatusCode = statusCode, Message = message };
+        }
+    }
+}
---
+++ Core/MiniECommerce.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace MiniECommerce.Application
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
