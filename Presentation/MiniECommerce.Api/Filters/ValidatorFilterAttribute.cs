807773149acbd2dff164c262d9d344115c4f01fb
Guven Boydak <gvn.boydak@gmail.com>
2022-10-28 16:31:12 +0300

    added custom validation filter

---
+++ Presentation/MiniECommerce.Api/Filters/ValidatorFilterAttribute.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MiniECommerce.Application;
+
+namespace MiniECommerce.Persistance
+{
+    //FluentValidation default filter yerine kendi filterimizi olusturduk.
+    public class ValidatorFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                //ModelState.Values dan errorundan errorMesajı alıp List<string> tipindeki errors a atıyoruz.
+                List<string> errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+
+                //Contextden Gelen hataları CustomResponseDto ile  clienta dto olarak döndüruyoruz.
+                context.Result = new BadRequestObjectResult(CustomResponseDto<NoContentDto>.Fail(404, errors, "Hatalı işlem"));
+
+            }
+        }
+    }
+}
