ca922975645d04463d778d4a848c3d9b0ecb6c0b
Guven Boydak <gvn.boydak@gmail.com>
2022-10-28 16:13:29 +0300

    added CheckPassword extension

---
+++ Core/MiniECommerce.Application/Extensions/CheckPassword.cs
@@ -0,0 +1,13 @@
+﻿namespace MiniECommerce.Application
+{
+    public static class CheckPassword
+    {
+        public static bool CheckingPassword(string newPassword, string confirmPassword)
+        {
+            if (string.Equals(newPassword, confirmPassword))
+                return true;
+            else
+                return false;
+        }
+    }
+}
