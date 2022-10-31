48ff82d198deed4e153db23b1c3531ab9b130908
Guven Boydak <gboydak@protel.com.tr>
2022-10-31 21:18:09 +0300

    Add aplication and persistance ServiceRegistration

--- Infrastructure/MiniECommerce.Persistance/ServiceRegistration.cs
+++ Infrastructure/MiniECommerce.Persistance/ServiceRegistration.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MiniECommerce.Application;
+using MiniECommerce.Domain;
+using MiniECommerce.Infrastructure;
+using MiniECommerce.Persistance.Services;
 
 namespace MiniECommerce.Persistance
 {
@@ -9,6 +14,45 @@ namespace MiniECommerce.Persistance
         {
             services.AddDbContext<MiniECommerceDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
 
+           
+
+            services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<>));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
+            services.AddScoped<IReadAppUserRepository, AppUserReadRepository>();
+            services.AddScoped<IWriteAppUserRepository, AppUserWriteRepository>();
+            services.AddScoped<IReadAppUserRoleRepository, AppUserRoleReadRepository>();
+            services.AddScoped<IWriteAppUserRoleRepository, AppUserRoleWriteRepositorty>();
+            services.AddScoped<IReadRoleRepository, RoleReadRepository>();
+            services.AddScoped<IWriteRoleRepository, RoleWriteRepository>();
+            services.AddScoped<IReadBrandRepository, BrandReadRepository>();
+            services.AddScoped<IWriteBrandRepository, BrandWriteRepository>();
+            services.AddScoped<IReadCategoryRepository, CategoryReadRepository>();
+            services.AddScoped<IWriteCategoryRepository, CategoryWriteRepository>();
+            services.AddScoped<IReadColorRepository, ColorReadRepository>();
+            services.AddScoped<IWriteColorRepository, ColorWriteRepository>();
+            services.AddScoped<IReadOfferRepository, OfferReadRepository>();
+            services.AddScoped<IWriteOfferRepository, OfferWriteRepository>();
+            services.AddScoped<IReadProductRepository, ProductReadRepository>();
+            services.AddScoped<IWriteProductRepository, ProductWriteRepository>();
+
+
+            services.AddScoped<IAppUserService, AppUserService>();
+            services.AddScoped<IAppUserRoleService, AppUserRoleService>();
+            services.AddScoped<IRoleService, RoleService>();
+            services.AddScoped<IBrandService, BrandService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IColorService, ColorService>();
+            services.AddScoped<IOfferService, OfferService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ITokenHelper, TokenHelper>();
+
+
+
+           
             return services;
         }
     }
