8f4d5dca2acd6cc2b0147c53c6eb12b03d7b0973
Guven Boydak <gvn.boydak@gmail.com>
2022-11-07 19:51:58 +0300

    Add EmailSender

---
+++ Core/MiniECommerce.Application/MailService/EmailSender.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using MiniECommerce.Domain;
+
+namespace MiniECommerce.Application
+{
+    public static class EmailSender
+    {
+
+        private static IConfiguration _configuration;
+
+        static EmailSender()
+        {
+
+            _configuration = Configuration;
+        }
+
+        public static IConfiguration Configuration
+        {
+            get
+            {
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json");
+                _configuration = builder.Build();
+                return _configuration;
+            }
+        }
+
+        public static async Task SendAsync(AppUser appUser,string subject,string body)
+        {
+            SmtpClient smtp = new SmtpClient();       
+            smtp.Host ="smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new NetworkCredential(_configuration.GetSection("Mail").Value,_configuration.GetSection("Password").Value);
+
+            MailMessage mail = new MailMessage(_configuration.GetSection("Mail").Value, appUser.Email);
+            mail.Subject = subject;
+            mail.Body = body;
+
+            try
+            {
+                await smtp.SendMailAsync(mail);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine($"Mesaj gönderilemedi.");
+            }
+        }
+    }
+}
