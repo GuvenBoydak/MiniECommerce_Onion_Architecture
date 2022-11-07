b59af24f71e79c8d27491c42baf60ed51dd6e9c5
Guven Boydak <gvn.boydak@gmail.com>
2022-11-07 19:59:39 +0300

    Add Hangfire  service registiration

--- Core/MiniECommerce.Application/ServiceRegistration.cs
+++ Core/MiniECommerce.Application/ServiceRegistration.cs
@@ -1,13 +1,32 @@
-﻿using MediatR;
+﻿using Hangfire;
+using Hangfire.PostgreSql;
+using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MiniECommerce.Application
 {
     public static class ServiceRegistration
     {
-        public static void AddApplicationServices(this IServiceCollection collection)
+        public static void AddApplicationServices(this IServiceCollection services)
         {
-            collection.AddMediatR(typeof(ServiceRegistration)); 
+            services.AddMediatR(typeof(ServiceRegistration));
+            
+            ServiceProvider provider = services.BuildServiceProvider();
+            IConfiguration _configuration = provider.GetService<IConfiguration>();
+
+            services.AddHangfire(configuration => configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_170)
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UsePostgreSqlStorage(_configuration.GetSection("HangfirePostreSql").Value, new PostgreSqlStorageOptions
+                {
+                    TransactionSynchronisationTimeout = TimeSpan.FromMinutes(5),
+                    InvisibilityTimeout = TimeSpan.FromMinutes(5),
+                    QueuePollInterval = TimeSpan.FromMinutes(5),
+                }));
+
+            services.AddHangfireServer();
         }
     }
 }
